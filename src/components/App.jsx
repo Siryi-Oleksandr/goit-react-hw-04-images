cb4fbd35437762873484d53370e22d587bffee3e
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-03-05 21:41:15 +0200

    app go to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { fetchImagesWithQuery, handleFetchData } from '../services/api';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -8,87 +8,68 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Error } from './Error/Error';
 
-export class App extends PureComponent {
-  state = {
-    query: '',
-    totalPages: 0,
-    page: 1,
-    images: [],
-    error: false,
-    status: 'idle',
-  };
+export function App() {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(false);
+  const [status, setStatus] = useState('idle');
 
-  async componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
+  useEffect(() => {
+    if (query === '') {
+      return setStatus('idle');
+    }
 
-    if (prevState.query !== query) {
-      this.setState({ status: 'pending', page: 1 });
+    setStatus('pending');
+
+    async function fetchData() {
       try {
-        const data = await fetchImagesWithQuery(query, 1);
+        const data = await fetchImagesWithQuery(query, page);
 
         // when bad request
         if (data.images.length === 0) {
-          toast.info(`"${query}" not found!`);
+          setImages([]);
+          setTotalPages(0);
+          setStatus('not found');
 
-          return this.setState({
-            images: [],
-            status: 'not found',
-            totalPages: 0,
-          });
+          return toast.info(`"${query}" not found!`);
         }
 
         const handleImages = handleFetchData(data.images);
-        this.setState({
-          images: handleImages,
-          status: 'resolved',
-          totalPages: data.totalPages,
-        });
+        setImages(prevImages => [...prevImages, ...handleImages]);
+        setStatus('resolved');
+        setTotalPages(data.totalPages);
       } catch (error) {
-        this.setState({ error, status: 'rejected' });
-        toast.error(`$Something went wrong`);
+        setError(error);
+        setStatus('rejected');
+        toast.error(`Something went wrong`);
       }
     }
 
-    if (prevState.page !== page && page !== 1) {
-      this.setState({ status: 'pending' });
-      try {
-        const data = await fetchImagesWithQuery(query, page);
-        const handleImages = handleFetchData(data.images);
-        this.setState(({ images }) => ({
-          images: [...images, ...handleImages],
-          status: 'resolved',
-          totalPages: data.totalPages,
-        }));
-      } catch (error) {
-        this.setState({ error, status: 'rejected' });
-        toast.error(`$Something went wrong`);
-      }
-    }
-  }
+    fetchData();
+  }, [query, page]);
 
-  handleSubmit = query => {
-    this.setState({ query });
+  const handleSubmit = query => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { images, status, page, totalPages, error } = this.state;
-    const availablePages = totalPages > page;
+  const availablePages = totalPages > page;
 
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleSubmit} />
-        {status === 'pending' && <Loader />}
-        {images.length > 0 && <ImageGallery images={images} />}
-        {availablePages && <Button onLoadMore={this.loadMore} />}
-        {status === 'rejected' && <Error error={error.message} />}
-        <ToastContainer position="top-right" autoClose={2500} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Searchbar onSubmit={handleSubmit} />
+      {status === 'pending' && <Loader />}
+      {images.length > 0 && <ImageGallery images={images} />}
+      {availablePages && <Button onLoadMore={loadMore} />}
+      {status === 'rejected' && <Error error={error.message} />}
+      <ToastContainer position="top-right" autoClose={2500} />
+    </div>
+  );
 }
